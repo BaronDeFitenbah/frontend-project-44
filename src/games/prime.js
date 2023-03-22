6e61445ae2aee455e5ffdfb8a4b1b53d70a64c93
BaronDeFitenbah <119754277+BaronDeFitenbah@users.noreply.github.com>
2023-03-22 22:49:38 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,18 +3,16 @@ import runEngine from '../index.js';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const generateRound = () => {
-  const isPrime = (number) => {
-    // here we could add more checks like number % 5
-    // but have no needs as our range of numbers is small (aren't we?..)
-    if (number < 4) return 'yes';
-    if (number % 2 === 0) return 'no';
-    for (let divider = 2; divider <= (number / 2); divider += 1) {
-      if (number % divider === 0) return 'no';
-    }
-    return 'yes';
-  };
+const isPrime = (number) => {
+  if (number < 4) return 'yes';
+  if (number % 2 === 0) return 'no';
+  for (let divider = 2; divider <= (number / 2); divider += 1) {
+    if (number % divider === 0) return 'no';
+  }
+  return 'yes';
+};
 
+const generateRound = () => {
   const question = getRandomInRange(1, 252);
   const answer = isPrime(question);
 
